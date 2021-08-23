defb494c4898cf9a1b8166497197aceb0f23e21a
VasiliyHadzheha <88691069+VasiliyHadzheha@users.noreply.github.com>
2021-08-24 02:44:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
+    let result = '';
+    if (expr.length % 10 !== 0) result = 0;
+     else {
+      for (let i = 0; i < expr.length; i += 10) {
+        let str = expr.slice(i, i +10);
+        if (str === '**********') result += ' ';
+        let acum = '';
+        let subStr = '';
+        for (let j = 0; j < str.length; j += 2) {
+          subStr = str[j] + str[j + 1];
+          if (subStr === '10') acum += '.';
+          if (subStr === '11') acum += '-';
+          }
+        for (let key in MORSE_TABLE) {
+          if (key === acum) result += MORSE_TABLE[key];
+        }
+      }
+    }
+    return result;
+  }
+  module.exports = {
     decode
-}
+  }
